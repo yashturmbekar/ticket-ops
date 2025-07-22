7f46b83a68fe56a730095cd9292c8571af44b4cb
Yash <yash.turmbekar@quasardigital.com>
2025-07-23 03:28:37 +0530

    style: Update TicketDetailsPageProfessional layout and add compact button styles

--- src/pages/TicketDetailsPageProfessional.tsx
+++ src/pages/TicketDetailsPageProfessional.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { getTicketById, getTicketComments } from "../services/ticketService";
 import {
   FaUser,
@@ -429,8 +429,7 @@ const TicketDetailsPageProfessional: React.FC = () => {
             </p>
           </div>
         </div>
-        <div className="create-page-actions">
-        </div>
+        <div className="create-page-actions"></div>
       </div>
 
       <div className="page-content">
